6be6d802d63ba3cc0d23e246ecd3e27a7376faba
Bis92 <bis920309@gmail.com>
2023-02-12 15:58:59 +0900

    youtube main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import VideoItems from './components/VideoItems/VideoItems';
+// import path from 'path';
 
 function App() {
+  const youtubeAPIkey = process.env.REACT_APP_KEY;
+  const [videos, setVideos] = useState([]);
+
+  const getHotVideos = async () => {
+    await fetch('data/hotVideos.json')
+    // await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${youtubeAPIkey}`)
+      .then(res => res.json())
+      .then((data) => setVideos(data.items))
+      .catch((err) => {
+        console.log('get hot videos err: ', err);
+      })
+  }
+
+  useEffect(() => {
+    getHotVideos();
+  }, []);
+
   return (
     <div className="App">
       <Header/>
+      <VideoItems
+        videoItems={videos}
+      />
     </div>
   );
 }
---
+++ src/components/VideoItems/VideoItems.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './VideoItems.module.css';
+import { format } from 'timeago.js';
+
+export default function VideoItems({ videoItems }) {
+    const numFormatter = new Intl.NumberFormat('ko', { notation: 'compact'});
+    const dayFormatter = new Intl.RelativeTimeFormat('ko', { numeric: 'auto' });
+    const today = new Date();
+    return (
+        <div className={styles.items}>
+            {videoItems.map((videoDetail) => <div className={styles.itemDetail}>
+                    <img src={`${videoDetail.snippet.thumbnails.medium.url}`} style={{ width: '100%', borderRadius: '1rem'}} />
+                    <span>{videoDetail.snippet.channelTitle}</span>
+                    <div>
+                        <span>조회수 {numFormatter.format(videoDetail.statistics.viewCount)} ● {format(new Date(videoDetail.snippet.publishedAt), 'ko')}</span>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
