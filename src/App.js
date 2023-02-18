301c23e2963371d3aaadcf2eb808e2bb39288124
Bis92 <bis920309@gmail.com>
2023-02-18 14:22:23 +0900

    main page(vidoes) completed

--- src/App.js
+++ src/App.js
@@ -7,10 +7,21 @@ import VideoItems from './components/VideoItems/VideoItems';
 function App() {
   const youtubeAPIkey = process.env.REACT_APP_KEY;
   const [videos, setVideos] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
-  const getHotVideos = async () => {
-    await fetch('data/hotVideos.json')
-    // await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${youtubeAPIkey}`)
+  const handleSearchVideos = async (inputValue) => {
+    setSearchValue(inputValue);
+    console.log(searchValue, inputValue);
+  }
+
+  const getHotVideos = async (searchItem) => {
+    let url;
+    if(searchItem){
+      url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&chart=mostPopular&maxResults=25&q=${searchItem}&key=${youtubeAPIkey}`
+    } else {
+      url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${youtubeAPIkey}`
+    }
+    await fetch(url)
       .then(res => res.json())
       .then((data) => setVideos(data.items))
       .catch((err) => {
@@ -18,13 +29,24 @@ function App() {
       })
   }
 
+  const initVideos = async () => {
+    getHotVideos();
+  }
+
   useEffect(() => {
     getHotVideos();
   }, []);
 
+  useEffect(() => {
+    getHotVideos(searchValue);
+  }, [searchValue])
+
   return (
     <div className="App">
-      <Header/>
+      <Header
+        handleSearchVideos={handleSearchVideos}
+        initVideos={initVideos}
+      />
       <VideoItems
         videoItems={videos}
       />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiFillYoutube } from "react-icons/ai";
 import { BsSearch } from "react-icons/bs";
 import styles from './Header.module.css';
 
-export default function Header() {
+export default function Header({ handleSearchVideos, initVideos }) {
+
+    const [searchItem, setSearchItem] = useState('');
+
+    const handleSubmit = (e) => {
+        if(e){
+            e.preventDefault();
+        }
+        handleSearchVideos(searchItem);
+    }
+
+    const handleChange = (e) => {
+        setSearchItem(e.target.value);
+    }
+
     return <header className={styles.header}>
-        <span className={styles.logo_and_name}>
+        <span className={styles.logo_and_name} onClick={() => initVideos()}>
             <span className={styles.logo}><AiFillYoutube/></span>
             <span className={styles.youtube_name}>Youtube</span>
         </span>
-        <form className={styles.input_form}>
-            <input className={styles.input}></input>
+        <form className={styles.input_form} onSubmit={handleSubmit}>
+            <input className={styles.input} onChange={handleChange}></input>
             <button className={styles.search_btn}>
                 <span className={styles.search_btn_icon}>
                     <BsSearch />
